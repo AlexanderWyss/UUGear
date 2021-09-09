37f40223215f4ded1aedee3094c5af3bcf85d730
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-09-09 19:35:27 +0200

    make ./libUUGear.so relative

--- RaspberryPi/src/UUGear.py
+++ RaspberryPi/src/UUGear.py
@@ -1,43 +1,45 @@
- #
- # UUGear Solution: extend your Raspberry Pi with Arduino
- #
- # Author: Shawn (shawn@uugear.com)
- #
- # Copyright (c) 2014 UUGear s.r.o.
- #
- #######################################################################
- #  This file is part of UUGear Solution: 
- #  http://www.uugear.com/uugear-rpi-arduino-solution/
- #  
- #  UUGear Solution is free software: you can redistribute it and/or modify
- #  it under the terms of the GNU Lesser General Public License as published
- #  by the Free Software Foundation, either version 3 of the License, or
- #  (at your option) any later version.
- #  
- #  UUGear Solution is distributed in the hope that it will be useful,
- #  but WITHOUT ANY WARRANTY; without even the implied warranty of
- #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #  GNU Lesser General Public License for more details.
- #  
- #  You should have received a copy of the GNU Lesser General Public License
- #  along with UUGear Solution.  If not, see <http://www.gnu.org/licenses/>.
- #
- #######################################################################
- 
+#
+# UUGear Solution: extend your Raspberry Pi with Arduino
+#
+# Author: Shawn (shawn@uugear.com)
+#
+# Copyright (c) 2014 UUGear s.r.o.
+#
+#######################################################################
+#  This file is part of UUGear Solution:
+#  http://www.uugear.com/uugear-rpi-arduino-solution/
+#
+#  UUGear Solution is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  UUGear Solution is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with UUGear Solution.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+import os
 from ctypes import CDLL, Structure, POINTER, byref, c_char, c_int, c_float, c_char_p
 
 UUGEAR_ID_MAX_LENGTH = 1024
 
-uugearlib = CDLL('./libUUGear.so')
+uugearlib = CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libUUGear.so'))
+
 
 class UUGearDeviceProfile(Structure):
     _fields_ = [
-    	('id', (c_char * UUGEAR_ID_MAX_LENGTH)), 
-    	('clientId', c_int), 
-    	('fd', c_int), 
-    	('in', c_int), 
-    	('out', c_int)
+        ('id', (c_char * UUGEAR_ID_MAX_LENGTH)),
+        ('clientId', c_int),
+        ('fd', c_int),
+        ('in', c_int),
+        ('out', c_int)
     ]
+
 
 uugearlib.setShowLogs.restype = None
 uugearlib.setShowLogs.argtypes = [c_int]
@@ -92,90 +94,89 @@ uugearlib.detachUUGearDevice.argtypes = [POINTER(UUGearDeviceProfile)]
 
 
 class UUGearDevice(object):
-	
-	devProfile = None
+    devProfile = None
 
-	@staticmethod
-	def setShowLogs(show):
-		uugearlib.setShowLogs(show)
-	
-	def __init__(self, id):
-		uugearlib.setupUUGear()
-		self.devProfile = uugearlib.attachUUGearDevice(id)
-		
-	def isValid(self):
-		return self.devProfile != None and self.devProfile.fd != -1
-	
-	def detach(self):
-		if self.isValid():
-			uugearlib.detachUUGearDevice(byref(self.devProfile))
-			self.devProfile = None	
-		
-	def stopDaemon(self):
-		uugearlib.cleanupUUGear()
-	
-	def setPinModeAsOutput(self, pin):
-		if self.isValid():
-			uugearlib.setPinModeAsOutput(byref(self.devProfile), pin)
-		
-	def setPinModeAsInput(self, pin):
-		if self.isValid():
-			uugearlib.setPinModeAsInput(byref(self.devProfile), pin)
-		
-	def setPinHigh(self, pin):
-		if self.isValid():
-			uugearlib.setPinHigh(byref(self.devProfile), pin)
-			
-	def setPinLow(self, pin):
-		if self.isValid():
-			uugearlib.setPinLow(byref(self.devProfile), pin)
-			
-	def getPinStatus(self, pin):
-		if self.isValid():
-			return uugearlib.getPinStatus(byref(self.devProfile), pin)
-		else:
-			return -1
+    @staticmethod
+    def setShowLogs(show):
+        uugearlib.setShowLogs(show)
 
-	def analogWrite(self, pin, value):
-		if self.isValid():
-			uugearlib.analogWrite(byref(self.devProfile), pin, value)
+    def __init__(self, id):
+        uugearlib.setupUUGear()
+        self.devProfile = uugearlib.attachUUGearDevice(id)
 
-	def analogRead(self, pin):
-		if self.isValid():
-			return uugearlib.analogRead(byref(self.devProfile), pin)
-		else:
-			return -1
-			
-	def analogReference(self, refType):
-		if self.isValid():
-			uugearlib.analogReference(byref(self.devProfile), refType)
+    def isValid(self):
+        return self.devProfile != None and self.devProfile.fd != -1
 
-	def attachServo(self, pin):
-		if self.isValid():
-			uugearlib.attachServo(byref(self.devProfile), pin)
-			
-	def writeServo(self, pin, angle):
-		if self.isValid():
-			uugearlib.writeServo(byref(self.devProfile), pin, angle)
+    def detach(self):
+        if self.isValid():
+            uugearlib.detachUUGearDevice(byref(self.devProfile))
+            self.devProfile = None
 
-	def readServo(self, pin):
-		if self.isValid():
-			return uugearlib.readServo(byref(self.devProfile), pin)
-		else:
-			return -1
-			
-	def detachServo(self, pin):
-		if self.isValid():
-			uugearlib.detachServo(byref(self.devProfile), pin)
+    def stopDaemon(self):
+        uugearlib.cleanupUUGear()
 
-	def readDHT(self, pin):
-		if self.isValid():
-			return uugearlib.readDHT(byref(self.devProfile), pin)
-		else:
-			return -1
-			
-	def readSR04(self, trigPin, echoPin):
-		if self.isValid():
-			return uugearlib.readSR04(byref(self.devProfile), trigPin, echoPin)
-		else:
-			return -1
+    def setPinModeAsOutput(self, pin):
+        if self.isValid():
+            uugearlib.setPinModeAsOutput(byref(self.devProfile), pin)
+
+    def setPinModeAsInput(self, pin):
+        if self.isValid():
+            uugearlib.setPinModeAsInput(byref(self.devProfile), pin)
+
+    def setPinHigh(self, pin):
+        if self.isValid():
+            uugearlib.setPinHigh(byref(self.devProfile), pin)
+
+    def setPinLow(self, pin):
+        if self.isValid():
+            uugearlib.setPinLow(byref(self.devProfile), pin)
+
+    def getPinStatus(self, pin):
+        if self.isValid():
+            return uugearlib.getPinStatus(byref(self.devProfile), pin)
+        else:
+            return -1
+
+    def analogWrite(self, pin, value):
+        if self.isValid():
+            uugearlib.analogWrite(byref(self.devProfile), pin, value)
+
+    def analogRead(self, pin):
+        if self.isValid():
+            return uugearlib.analogRead(byref(self.devProfile), pin)
+        else:
+            return -1
+
+    def analogReference(self, refType):
+        if self.isValid():
+            uugearlib.analogReference(byref(self.devProfile), refType)
+
+    def attachServo(self, pin):
+        if self.isValid():
+            uugearlib.attachServo(byref(self.devProfile), pin)
+
+    def writeServo(self, pin, angle):
+        if self.isValid():
+            uugearlib.writeServo(byref(self.devProfile), pin, angle)
+
+    def readServo(self, pin):
+        if self.isValid():
+            return uugearlib.readServo(byref(self.devProfile), pin)
+        else:
+            return -1
+
+    def detachServo(self, pin):
+        if self.isValid():
+            uugearlib.detachServo(byref(self.devProfile), pin)
+
+    def readDHT(self, pin):
+        if self.isValid():
+            return uugearlib.readDHT(byref(self.devProfile), pin)
+        else:
+            return -1
+
+    def readSR04(self, trigPin, echoPin):
+        if self.isValid():
+            return uugearlib.readSR04(byref(self.devProfile), trigPin, echoPin)
+        else:
+            return -1
